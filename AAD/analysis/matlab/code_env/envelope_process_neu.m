2d348f471532561160f3ca87f3f1f128af5a2bd1
Xiran Xu <48015859+xuxiran@users.noreply.github.com>
2024-03-11 20:30:31 +0800

    Update envelope_process_neu.m

--- AAD/analysis/matlab/code_env/envelope_process_neu.m
+++ AAD/analysis/matlab/code_env/envelope_process_neu.m
@@ -1,20 +1,17 @@
 clear;clc;
 
-%sbnum = 16;
-
-
 dim_tr =59;
-% load('Envelope_all.mat');
+
 load('./space_num.mat');
 load('./space_envall.mat')
-% space_env
-att_rand = [1 1 1 1 1 1 1 1 1 1];%1是欢乐颂的包络，2是summer的
+
+att_rand = [1 1 1 1 1 1 1 1 1 1];
 unatt_rand = [2 2 2 2 2 2 2 2 2 2];
 
 Envelope_att = space_env(space_num(:,1),:);
-% Envelope_unatt = peom_env(unatt_rand,:);
 
-%计算250ms的解码器
+
+
 if 1
 
     nowdir = cd;
@@ -33,14 +30,14 @@ if 1
     sblist(1:2) = [];
 
     sbnum = size(sblist,1);
-    decode_att = zeros(sbnum,40,dim);%2个被试，每个被试4*10条件
+    decode_att = zeros(sbnum,40,dim);
     decode_unatt = zeros(sbnum,40,dim);
 
     for sb = 1:sbnum
         sbname = sblist(sb).name;
         sbdir = [DataDir filesep sbname];
 
-        for tr = 1:40 % 注意的试次；21-30&31-40
+        for tr = 1:40
             
 
             disp(['  sb:' num2str(sb) '  tr:' num2str(tr)]);
@@ -52,21 +49,17 @@ if 1
             eeg = eeg';
             eeg = zscore(eeg);
             
-            env_att = Envelope_att(tr,:)';%%注意一下修改试次后这里也要修改
-%             env_unatt = Envelope_unatt(tr-20,:)';%%注意一下修改试次后这里也要修改
+            env_att = Envelope_att(tr,:)';
 
 
             X = [ones(size(eeg)),lagGen(eeg,min(-Lags):max(-Lags))];
 
             XX = X'*X;
             XYatt = X'*env_att;
-%             XYunatt = X'*env_unatt;
-            %对注意音频的解码器
+
             d2_att = (XX+4096*M)\XYatt;
-            %对非注意音频的解码器
-            %d2_unatt = (XX+4096*M)\XYunatt;
-            decode_att(sb,tr,:) = d2_att';%%注意一下修改试次后这里也要修改
-            %decode_unatt(sb,tr-20,:) = d2_unatt';%%注意一下修改试次后这里也要修改
+            
+            decode_att(sb,tr,:) = d2_att';
         end
 
 
@@ -75,7 +68,7 @@ if 1
 end
 
 
-%第二部分：试验解码结果（500ms）
+
 if 1
 
     nowdir = cd;
@@ -95,7 +88,7 @@ if 1
     sblist(1:2) = [];
 
     res_raw =zeros(16,7);
-    %两种解码器
+
     load('envelope_decoder_nue_05_16_59.mat');
     C_att_raw = zeros(sbnum,40);
     C_unatt_raw = zeros(sbnum,40);
@@ -120,8 +113,8 @@ if 1
             eeg = zscore(eeg);
 
            
-            env_att = Envelope_att(tr,:)';%%注意一下修改试次后这里也要修改
-            env_unatt = space_env(unattnum,:)';%%注意一下修改试次后这里也要修改
+            env_att = Envelope_att(tr,:)';
+            env_unatt = space_env(unattnum,:)';
 
             
             X = [ones(size(eeg)),lagGen(eeg,min(-Lags):max(-Lags))];
@@ -141,7 +134,7 @@ if 1
 
 
     end
-     %检验最后结果
+
         de_raw = gt(C_att_raw,C_unatt_raw);
         
      res_raw(:,i) = mean(mean(de_raw,2),3);
@@ -154,7 +147,7 @@ if 1
 end
 
 
-%lagGen函数
+
 function xLag = lagGen(x,lags)
 xLag = zeros(size(x,1),size(x,2)*length(lags));
 
